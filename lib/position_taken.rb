5f0fe3341b219c99d33c8f4f2f5fe785ac87e374
David Morphew <morphewd@umich.edu>
2020-07-21 20:44:41 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,6 @@
 # code your #position_taken? method here!
-board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
 def position_taken?(board, index)
-  if board[index].to_s == "X" || "O" 
-    puts "true"
+  if board[index] == " " || "" || nil
+    false
   end
 end
